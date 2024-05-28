c9665601c61a69300b0d1ac3c24f8e6c53c96bd0
Guilherme G. <ep.0.season.@gmail.com>
2024-05-28 17:27:46 -0300

    .\tiposDeVariáveis\

--- tiposDeVariáveis/src/TiposDeVariaveis.java
+++ tiposDeVariáveis/src/TiposDeVariaveis.java
@@ -1,10 +1,4 @@
 public class TiposDeVariaveis {
-    public double calcularAumento (double salario){
-        double novoSalario;
-        novoSalario = (salario*0.2);
-
-        return novoSalario;
-    }
     public static void main(String[] args) {
         String nome = "Guilherme";
         byte idade = 23;
@@ -21,9 +15,6 @@ public class TiposDeVariaveis {
         System.out.print(matricula + " - Tipo: long" + "\n");
         System.out.print(altura + " - Tipo: float" + "\n");
         System.out.print(salario + " - Tipo: double");
-
-        System.out.println("\n");
-        System.out.println("O novo salário será de: ");
     }
 }
 
