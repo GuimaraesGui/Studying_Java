9a01d79fd214bf8da803f615760aafe1dfa38d6b
Guilherme G. <ep.0.season.@gmail.com>
2024-05-31 21:57:02 -0300

    Att.

---
+++ Studying_Java/CalcularNota/CalcularNotaFacul.java
@@ -0,0 +1,15 @@
+//Regra para o cálculo da média: [#00] - N1 e N2 = ADx2 + APx8; N = (N1 + N2)/2; Se N >=6 então NF = N senão NF = [(MAIOR (N1, N2) + AP3]/2
+
+/*
+ * Matérias:
+ * álgebra linear
+ * administração
+ * humanidades e ciências sociais
+ * desenho
+ * estatística e probabilidade
+ */
+public class CalcularNotaFacul{
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
--- Studying_Java/src/operadores.java
+++ Studying_Java/src/operadores.java
@@ -1,6 +1,32 @@
 public class operadores {
-    public static void main(String [] args){
-        double variancia;
-        int media;
+    public static float calcularMedia(int a, int b, int c) {
+        float media = (a+b+c)/3F;
+        
+        return media;
+    }
+
+    public static float somarDesvios(int a, int b, int c){
+        float desvio1 = (calcularMedia(a, b, c)-a)*(calcularMedia(a, b, c)-a);
+        float desvio2 = (calcularMedia(a, b, c)-b)*(calcularMedia(a, b, c)-b);
+        float desvio3 = (calcularMedia(a, b, c)-c)*(calcularMedia(a, b, c)-c);
+        float desvios = desvio1 + desvio2 + desvio3;
+
+        return desvios;
+    }
+
+    public static float calcularVariancia(int a, int b, int c) {
+        float variancia = somarDesvios(a, b, c)/2;
+
+        return variancia;
+    }
+
+    public static void main(String[] args) {
+        int a = 8, b = 15, c = 23;
+
+        System.out.println("Média: " + calcularMedia(a, b, c));
+        System.out.println("\n");
+        System.err.println("Soma dos desvio: " + somarDesvios(a, b, c));
+        System.out.println("\n");
+        System.out.println("Variância: " + calcularVariancia(a, b, c));
     }
 }
