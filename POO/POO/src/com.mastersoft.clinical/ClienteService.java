30c5c5ba353fe9d66583be4f5172c513b002e2b6
Guilherme G. <ep.0.season.@gmail.com>
2024-07-04 22:31:50 -0300

    "Att."

--- POO/POO/src/com.mastersoft.clinical/ClienteService.java
+++
@@ -1,5 +0,0 @@
-public class ClienteService {
-    public static void main(String[] args) {
-        System.out.println("Naturgy");
-    }
-}
--- POO/POO/src/com.mastersoft.clinical/Exame.java
+++
@@ -1,5 +0,0 @@
-public class Exame {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
